cc6e5d6b07b17290788158cf6a37e442df7bb8a5
Sean Johnson <seanjohnson220@gmail.com>
2021-01-20 17:33:25 -0500

    queue module updated

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,22 +1,62 @@
 class Queue {
   constructor() {
     // Set initial data.
+    this.first = null;
+    this.last = null;
   }
 
   enqueue(data) {
-    // Add some data to the queue.
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
   }
 
   dequeue() {
-    // Remove some data from the queue.
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = this.first.next
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
   }
 
   show() {
-    // Return the next item in the queue.
+    if (this.first === null) {
+      return "No items remaining in the Queue"
+    }
+    if (this.first) {
+      return this.first;
+    }
   }
 
   all() {
-    // Return all items in the queue.
+    let current = this.first;
+    let allNodes = [];
+
+    while (current != null) {
+      allNodes.push(current);
+      current = current.next;
+    }
+    return allNodes;
+  }
+}
+
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
